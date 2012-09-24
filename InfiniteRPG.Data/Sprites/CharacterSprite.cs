079d614486292fb986d9908dc62f20437d86a008
Alec Lanter <kintar1900@gmail.com>
2012-09-24 12:33:56 -0500

    WIP on sprites

---
+++ InfiniteRPG.Data/Sprites/CharacterSprite.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace InfiniteRPG.Data.Sprites
+{
+    public enum CharacterState
+    {
+        Moving,
+        Idle
+    }
+
+    public enum CharacterFacing
+    {
+        South,
+        West,
+        East,
+        North,
+    }
+
+    public class CharacterSprite
+    {
+        public Vector2 Position { get; set; }
+        public Texture2D Sprite { get; set; }
+        public CharacterState State { get; set; }
+        public CharacterFacing Facing { get; set; }
+
+        public CharacterSprite(Texture2D sprite)
+        {
+            Sprite = sprite;
+            Position = Vector2.Zero;
+            State = CharacterState.Idle;
+            Facing = CharacterFacing.South;
+        }
+    }
+}
